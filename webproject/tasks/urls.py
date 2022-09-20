a1e4f762050808dd47e5b9a301cc7032df5890da
Yugai Denis <yugai_denis@mail.ru>
2022-09-20 13:24:19 +0600

    Именение файла task_create.html + Добавление пути edit в urls.py + Добавление edit_view в tasks.py + Добавление шаблона task_edit.html

--- webproject/tasks/urls.py
+++ webproject/tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from tasks.views.base import index_view
-from tasks.views.tasks import add_view, del_view
+from tasks.views.tasks import add_view, del_view, edit_view
 
 urlpatterns = [
     path('', index_view),
     path('add/', add_view),
-    path('del/', del_view)
+    path('del/', del_view),
+    path('edit/', edit_view)
 ]
--- webproject/tasks/views/tasks.py
+++ webproject/tasks/views/tasks.py
@@ -24,3 +24,23 @@ def del_view(request):
         task = Task.objects.get(pk=id_task)
         task.delete()
         return redirect('/')
+
+
+def edit_view(request):
+    if request.method == 'GET':
+        id_task = request.GET.get('id')
+        task = Task.objects.get(pk=id_task)
+        choices = Task.CHOICES
+        context = {
+            "task": task,
+            'choices': choices
+        }
+        return render(request, 'task_edit.html', context=context)
+    task = Task.objects.get(id=request.GET.get('id'))
+    task.description = request.POST.get('description')
+    task.status = request.POST.get('status')
+    task.date_deadline = request.POST.get('date_deadline')
+    task.save()
+    
+    return redirect('/')
+    
